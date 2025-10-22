fa4e5b65ebcac7e11deba3866a7fa64047bc338b
Eliazynho <m10elias10@gmail.com>
2025-10-22 09:29:57 -0300

    refactor: Fix ESLint violations and remove redundant comments

--- src/http-exception/http-exception.filter.spec.ts
+++ src/http-exception/http-exception.filter.spec.ts
@@ -1,7 +1,6 @@
 import { AllExceptionsFilter } from './http-exception.filter';
 
 describe('AllExceptionsFilter', () => {
-  // 👈 Opcional: corrija o nome aqui para consistência
   it('should be defined', () => {
     expect(new AllExceptionsFilter()).toBeDefined();
   });
